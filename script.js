16d74b54e6ec2b5403db0813d08f0a07c798bb55
Glitch (battle-possible-suggestion) <none>
2021-03-05 19:25:07 +0000

    🎪🙇 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,144 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+//global constants
+const clueHoldTime = 1000;
+const cluePauseTime = 333;
+const nextClueWaitTime = 1000;
+
+//Global variables
+var pattern = [2,2,4,3,2,1,2,4];
+var progress = 0;
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;
+var guessCounter = 0;
+
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit");
+}
+function clearButton(btn){
+  document.getElementById("button" + btn).classList.remove("lit");
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  //set delay to initial wait time
+  guessCounter = 0;
+  let delay = nextClueWaitTime;
+  for(let i = 0; i <= progress; i++){
+    console.log("play single clue: " + pattern[i] + " in "+ delay + "ms");
+    setTimeout(playSingleClue, delay,pattern[i]);
+    delay += clueHoldTime;
+    delay += cluePauseTime;
+    
+  }
+}
+
+
+function startGame(){
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  playClueSequence();
+}
+
+function stopGame(){
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+
+}
+
+function guess(btn){
+  console.log("test")
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+      }
+
+  
+   if(pattern[guessCounter] == btn){
+    if(guessCounter == progress){
+    if(progress == pattern.length - 1){
+      winGame();
+          }else{
+            
+            progress++;
+            playClueSequence();
+          }
+  }else{
+    guessCounter++;
+  }
+}else{
+  loseGame();
+}
+}
+    
+
+
+
+
+
+
+
+//checking the user response
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+
+
+function winGame(){
+  stopGame();
+  alert("Game Over. You won.");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 261.6,
+  2: 329.6,
+  3: 392,
+  4: 466.2
+}
+
+//sound library
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
